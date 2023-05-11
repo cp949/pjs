94b0a046f2a1501c39d1adccaf1e4c66a5b864c7
cp949 <softdada@gmail.com>
2023-05-11 21:16:02 +0900

    apply type guard

--- src/pstring/isNotBlank.ts
+++ src/pstring/isNotBlank.ts
@@ -1,5 +1,5 @@
 import { isBlank } from './isBlank'
 
-export function isNotBlank(str: string | undefined | null): boolean {
-    return !isBlank(str)
+export function isNotBlank(str: string | undefined | null): str is string {
+  return !isBlank(str)
 }
--- src/pstring/isNotEmpty.ts
+++ src/pstring/isNotEmpty.ts
@@ -1,3 +1,3 @@
-export function isNotEmpty(str: string | undefined | null): boolean {
-    return (str?.length ?? 0) > 0
+export function isNotEmpty(str: string | undefined | null): str is string {
+  return (str?.length ?? 0) > 0
 }
